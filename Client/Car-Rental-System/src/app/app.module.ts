de9a20dbbe09d2f3837e035c22436ad725994f47
Matheus Frej <matheusfrej@hotmail.com>
2022-08-29 00:53:43 -0300

    feedback page structure in progress

--- Client/Car-Rental-System/src/app/app.module.ts
+++ Client/Car-Rental-System/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FeedbackPageComponent } from './feedback-page/feedback-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomePageComponent
+    HomePageComponent,
+    FeedbackPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ Client/Car-Rental-System/src/app/feedback-page/feedback-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FeedbackPageComponent } from './feedback-page.component';
+
+describe('FeedbackPageComponent', () => {
+  let component: FeedbackPageComponent;
+  let fixture: ComponentFixture<FeedbackPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FeedbackPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FeedbackPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
